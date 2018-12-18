1550d0db238f295c86b9ab9e84c34adc78b24bef
Reed Rizzo <reed.rizzo@coatesgroup.com>
2018-12-18 13:22:35 -0600

    wtf

--- Routes/contentChangeRequest.js
+++ Routes/contentChangeRequest.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ContentChangeRequest = require('./../models/ContentChangeRequest')
 const contentChangeRequestRouter = express.Router()
 
-contentChangeRequestRouter.route('/wtf')
+contentChangeRequestRouter.route('/')
     .post((req, res) => {
         ContentChangeRequest.find()
             .then((changeRequests) => {
@@ -13,72 +13,72 @@ contentChangeRequestRouter.route('/wtf')
             })
     })
 
-contentChangeRequestRouter.route('/')
-    .post((req, res) => {
-        console.log('post worked')
-        const changeRequest = new ContentChangeRequest(req.query);
-        changeRequest.save();
-        console.log(changeRequest)
-        res.status(201).send(changeRequest)
-    }) 
+// contentChangeRequestRouter.route('/')
+//     .post((req, res) => {
+//         console.log('post worked')
+//         const changeRequest = new ContentChangeRequest(req.query);
+//         changeRequest.save();
+//         console.log(changeRequest)
+//         res.status(201).send(changeRequest)
+//     }) 
 
-contentChangeRequestRouter.route('/:id')
-    .get((req, res) => {
-        ContentChangeRequest.find({ _id: req.params.id })
-            .then((changeRequests) => {
-                res.json(changeRequests)
-            })  
-            .catch(err => {
-                console.log(err)
-            })
-    }) 
+// contentChangeRequestRouter.route('/:id')
+//     .get((req, res) => {
+//         ContentChangeRequest.find({ _id: req.params.id })
+//             .then((changeRequests) => {
+//                 res.json(changeRequests)
+//             })  
+//             .catch(err => {
+//                 console.log(err)
+//             })
+//     }) 
 
-contentChangeRequestRouter.route('/:id')
-    .put((req, res) => {
-        ContentChangeRequest
-            .findOneAndUpdate(req.params.id, req.query, {upsert:true}, (err, doc) => {
-                if (err) return res.send(500, { error: err });
-                res.json(doc)
-            })
-    })
+// contentChangeRequestRouter.route('/:id')
+//     .put((req, res) => {
+//         ContentChangeRequest
+//             .findOneAndUpdate(req.params.id, req.query, {upsert:true}, (err, doc) => {
+//                 if (err) return res.send(500, { error: err });
+//                 res.json(doc)
+//             })
+//     })
 
-contentChangeRequestRouter.route('/:id')
-    .delete((req, res) => {
+// contentChangeRequestRouter.route('/:id')
+//     .delete((req, res) => {
 
-        ContentChangeRequest
-            .findById(req.params.id)
-            .then((changeRequest) => {
-                if (!changeRequest) {
-                    res.status(500).json({
-                        success: false,
-                        message: `Request not found ${req.params.id}`,
-                        error: err
-                    })
-                    return true
-                }
+//         ContentChangeRequest
+//             .findById(req.params.id)
+//             .then((changeRequest) => {
+//                 if (!changeRequest) {
+//                     res.status(500).json({
+//                         success: false,
+//                         message: `Request not found ${req.params.id}`,
+//                         error: err
+//                     })
+//                     return true
+//                 }
 
-                changeRequest.remove(err => {
-                    if (err) {
-                        res.status(500).json({
-                            success: false,
-                            error: err
-                        })
-                    } else {
-                        res.status(201).json({
-                            success: true
-                        })
-                    }
-                })
+//                 changeRequest.remove(err => {
+//                     if (err) {
+//                         res.status(500).json({
+//                             success: false,
+//                             error: err
+//                         })
+//                     } else {
+//                         res.status(201).json({
+//                             success: true
+//                         })
+//                     }
+//                 })
                 
-            })
-            .catch(err => {
-                res.status(500).json({
-                    success: false,
-                    message: err.message,
-                    error: err
-                })
-            })
+//             })
+//             .catch(err => {
+//                 res.status(500).json({
+//                     success: false,
+//                     message: err.message,
+//                     error: err
+//                 })
+//             })
 
-    }) 
+//     }) 
 
 module.exports = contentChangeRequestRouter
