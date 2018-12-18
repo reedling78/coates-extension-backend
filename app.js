911f446531a752919f4082b48abc4223d23ff18b
Reed Rizzo <reed.rizzo@coatesgroup.com>
2018-12-18 12:37:16 -0600

    whatever

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 const express = require('express')
 const app = express()
-const ContentChangeRequest = require('./models/ContentChangeRequest')
 
 const contentChangeRequestRouter = require('./Routes/contentChangeRequest')
 
